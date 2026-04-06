50b456682ca4a43b4c61540330c3dd2842fcdd00
Samuel <toboncs@gmail.com>
2026-04-05 20:38:30 -0500

    Samus s a

--- src/application/controllers/address-controller.ts
+++ src/application/controllers/address-controller.ts
@@ -11,7 +11,7 @@ export const getAddress = async (req: AuthRequest, res: Response) => {
       return res.status(200).json({ ok: true, message: "Aun no has creado tu dirección", data: [] });
     }
 
-    //Prueba 2
+    //Prueba 2 3
     res.json({ ok: true, data: address });
   } catch (error) {
     console.error(error);
